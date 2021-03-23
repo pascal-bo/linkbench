435fbdab892d0da8c33829bd3dd681b7577a87f9
RobbyRed98 <r.roth@mail.de>
2021-03-23 17:37:15 +0100

    added LinkStoreDb2sql as LinkStore impl for DB2 with basic support for the LOAD Phase

---
+++ db2graph-setup.sql
@@ -0,0 +1,30 @@
+CREATE TABLE linkdb0.linktable
+(
+    id1        bigint  NOT NULL DEFAULT '0' REFERENCES linkdb0.nodetable,
+    id2        bigint  NOT NULL DEFAULT '0' REFERENCES linkdb0.nodetable,
+    link_type  bigint  NOT NULL DEFAULT '0',
+    visibility smallint     NOT NULL DEFAULT '0',
+    data       varbinary(255) NOT NULL,
+    time       bigint  NOT NULL DEFAULT '0',
+    version    bigint       NOT NULL DEFAULT '0',
+    PRIMARY KEY (link_type, id1, id2)
+);
+
+CREATE TABLE linkdb0.counttable
+(
+    id        bigint NOT NULL DEFAULT '0',
+    link_type bigint NOT NULL DEFAULT '0',
+    count     int         NOT NULL DEFAULT '0',
+    time      bigint NOT NULL DEFAULT '0',
+    version   bigint NOT NULL DEFAULT '0',
+    PRIMARY KEY (id, link_type)
+);
+
+CREATE TABLE linkdb0.nodetable
+(
+    id      bigint NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+    type    int       NOT NULL,
+    version numeric   NOT NULL,
+    time    int       NOT NULL,
+    data    text      NOT NULL
+);
--- pgsql-ddl.sql
+++ pgsql-ddl.sql
@@ -4,49 +4,56 @@ drop database if exists linkdb0;
 create database linkdb0 encoding='latin1' ;
 
 --drop user linkbench to create new one
-DROP USER  IF EXISTS linkbench;
+DROP
+USER  IF EXISTS linkbench;
 
 --	You may want to set up a special database user account for benchmarking:
-CREATE USER linkbench password 'linkbench';
+CREATE
+USER linkbench password 'linkbench';
 -- Grant all privileges on linkdb0 to this user
-GRANT ALL ON database linkdb0 TO linkbench;
+GRANT ALL
+ON database linkdb0 TO linkbench;
 
 --add Schema keep the same query style
-DROP SCHEMA IF EXISTS linkdb0 CASCADE; 
+DROP SCHEMA IF EXISTS linkdb0 CASCADE;
 CREATE SCHEMA linkdb0;
 
 --conn postgresql linkbench/password
 
 --FIXME:Need to make it partitioned by key id1 %16
-CREATE TABLE linkdb0.linktable (
-		id1 numeric(20) NOT NULL DEFAULT '0',
-		id2 numeric(20) NOT NULL DEFAULT '0',
-		link_type numeric(20) NOT NULL DEFAULT '0',
-		visibility smallint NOT NULL DEFAULT '0',
-		data varchar(255) NOT NULL DEFAULT '',
-		time numeric(20) NOT NULL DEFAULT '0',
-		version bigint NOT NULL DEFAULT '0',
-		PRIMARY KEY (link_type, id1,id2)
-		);
+CREATE TABLE linkdb0.linktable
+(
+    id1        numeric(20)  NOT NULL DEFAULT '0',
+    id2        numeric(20)  NOT NULL DEFAULT '0',
+    link_type  numeric(20)  NOT NULL DEFAULT '0',
+    visibility smallint     NOT NULL DEFAULT '0',
+    data       varchar(255) NOT NULL DEFAULT '',
+    time       numeric(20)  NOT NULL DEFAULT '0',
+    version    bigint       NOT NULL DEFAULT '0',
+    PRIMARY KEY (link_type, id1, id2)
+);
 
 -- this is index for linktable
-CREATE INDEX id1_type on linkdb0.linktable(id1,link_type,visibility,time,id2,version,data);
+CREATE
+INDEX id1_type on linkdb0.linktable(id1,link_type,visibility,time,id2,version,data);
 
-CREATE TABLE linkdb0.counttable (
-		id numeric(20) NOT NULL DEFAULT '0',
-		link_type numeric(20) NOT NULL DEFAULT '0',
-		count int NOT NULL DEFAULT '0',
-		time numeric(20) NOT NULL DEFAULT '0',
-		version numeric(20) NOT NULL DEFAULT '0',
-		PRIMARY KEY (id,link_type)
-		);
+CREATE TABLE linkdb0.counttable
+(
+    id        numeric(20) NOT NULL DEFAULT '0',
+    link_type numeric(20) NOT NULL DEFAULT '0',
+    count     int         NOT NULL DEFAULT '0',
+    time      numeric(20) NOT NULL DEFAULT '0',
+    version   numeric(20) NOT NULL DEFAULT '0',
+    PRIMARY KEY (id, link_type)
+);
 
-CREATE TABLE linkdb0.nodetable (
-		id BIGSERIAL NOT NULL,
-		type int NOT NULL,
-		version numeric NOT NULL,
-		time int NOT NULL,
-		data text NOT NULL,
-		PRIMARY KEY(id)
-		);
+CREATE TABLE linkdb0.nodetable
+(
+    id      BIGSERIAL NOT NULL,
+    type    int       NOT NULL,
+    version numeric   NOT NULL,
+    time    int       NOT NULL,
+    data    text      NOT NULL,
+    PRIMARY KEY (id)
+);
 
