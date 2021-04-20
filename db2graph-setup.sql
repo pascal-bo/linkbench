ac25a6a32ea4dc4dde4ed10eeba62dd0f32fdbb9
RobbyRed98 <r.roth@mail.de>
2021-04-20 16:53:06 +0200

    changed querying style for links from vertex to edges based added node, link, and count labels as parameters to the config file

--- db2graph-setup.sql
+++ db2graph-setup.sql
@@ -1,23 +1,26 @@
 CREATE TABLE linkdb0.linktable
 (
-    id1        bigint  NOT NULL DEFAULT '0', -- REFERENCES linkdb0.nodetable(id),
-    id2        bigint  NOT NULL DEFAULT '0', -- REFERENCES linkdb0.nodetable(id),
+    id1        bigint  NOT NULL DEFAULT '0',
+    id2        bigint  NOT NULL DEFAULT '0',
     link_type  bigint  NOT NULL DEFAULT '0',
     visibility smallint     NOT NULL DEFAULT '0',
     data       varchar(255) NOT NULL DEFAULT '',
     time       bigint  NOT NULL DEFAULT '0',
     version    bigint       NOT NULL DEFAULT '0',
-    PRIMARY KEY (link_type, id1, id2)
+    PRIMARY KEY (link_type, id1, id2),
+    FOREIGN KEY (id1) REFERENCES linkdb0.nodetable(id),  
+    FOREIGN KEY (id2) REFERENCES linkdb0.nodetable(id)
 );
 
 CREATE TABLE linkdb0.counttable
 (
-    id        bigint NOT NULL DEFAULT '0', -- REFERENCES linkdb0.nodetable(id),
+    id        bigint NOT NULL DEFAULT '0',
     link_type bigint NOT NULL DEFAULT '0',
     count     int         NOT NULL DEFAULT '0',
     time      bigint NOT NULL DEFAULT '0',
     version   bigint NOT NULL DEFAULT '0',
-    PRIMARY KEY (id, link_type)
+    PRIMARY KEY (id, link_type),
+    FOREIGN KEY (id) REFERENCES linkdb0.nodetable(id)
 );
 
 CREATE TABLE linkdb0.nodetable
